6d1b26ba3f4794bd0b93686ada79b8d3ce60935c
Nirmal Almara <nirmal.almara@outlook.com>
2019-06-15 21:43:23 +0530

    Initial golang gin work

--- sorcia.go
+++ sorcia.go
@@ -4,10 +4,13 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/public", "./public")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", "")
 	})
+
 	r.Run(":1937") // listen and serve on 0.0.0.0:1937
 }
