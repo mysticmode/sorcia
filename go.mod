0454a3f0e115a5e5ce3a96dea1236cb5fb6a710a
Nirmal Almara <hello@nlmara.com>
2019-06-27 16:34:00 +0530

    create account and register feature

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module sorcia
 go 1.12
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.4.0
 	github.com/lib/pq v1.1.1 // indirect
 	gopkg.in/ini.v1 v1.42.0
--- sorcia.go
+++ sorcia.go
@@ -3,13 +3,19 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net/http"
 	"path"
+	"time"
 
 	cError "sorcia/error"
+	"sorcia/middleware"
+	"sorcia/models/auth"
 	"sorcia/settings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
@@ -31,23 +37,113 @@ func main() {
 	cError.CheckError(err)
 	defer db.Close()
 
+	auth.CreateAccount(db)
+
+	r.Use(
+		middleware.CORSMiddleware(),
+		middleware.APIMiddleware(db),
+		// middleware.UserMiddleware(db),
+	)
+
 	// Gin handlers
-	r.GET("/", Home)
-	r.GET("/login", Login)
+	r.GET("/", GetHome)
+	r.GET("/login", GetLogin)
+	r.POST("/register", PostRegister)
 	r.GET("/host", GetHostAddress)
 
 	// Listen and serve on 1937
 	r.Run(fmt.Sprintf(":%s", conf.Server.HTTPPort))
 }
 
-// Home ...
-func Home(c *gin.Context) {
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func generateJWTToken(passwordHash string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{})
+	tokenString, err := token.SignedString([]byte(passwordHash))
+	return tokenString, err
+}
+
+func validateJWTToken(tokenString string, passwordHash string) (bool, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(passwordHash), nil
+	})
+
+	return token.Valid, err
+}
+
+// GetHome ...
+func GetHome(c *gin.Context) {
 	c.HTML(200, "index.html", "")
 }
 
-// Login ...
-func Login(c *gin.Context) {
+// GetLogin ...
+func GetLogin(c *gin.Context) {
 	c.HTML(200, "login.html", "")
+}
+
+// RegisterRequest struct
+type RegisterRequest struct {
+	Username string `form:"username" binding:"required"`
+	Email    string `form:"email" binding:"required"`
+	Password string `form:"password" binding:"required"`
+}
+
+// ErrorResponse struct
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+// PostRegister ...
+func PostRegister(c *gin.Context) {
+	var form RegisterRequest
+
+	if err := c.Bind(&form); err == nil {
+		// Generate password hash using bcrypt
+		passwordHash, err := hashPassword(form.Password)
+		cError.CheckError(err)
+
+		// Generate JWT token using the hash password above
+		token, err := generateJWTToken(passwordHash)
+		cError.CheckError(err)
+
+		db, ok := c.MustGet("db").(*sql.DB)
+		if !ok {
+			fmt.Println("Middleware db error")
+		}
+
+		rr := auth.CreateAccountStruct{
+			Username:     form.Username,
+			Email:        form.Email,
+			PasswordHash: passwordHash,
+			Token:        token,
+			IsAdmin:      1,
+		}
+
+		auth.InsertAccount(db, rr)
+
+		expiration := time.Now().Add(365 * 24 * time.Hour)
+		cookie := http.Cookie{Name: "sorcia-token", Value: token, Expires: expiration}
+		http.SetCookie(c.Writer, &cookie)
+
+		c.Redirect(http.StatusMovedPermanently, "/")
+	} else {
+		errorResponse := &ErrorResponse{
+			Error: err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, errorResponse)
+	}
 }
 
 // GetHostAddress returns the URL address
