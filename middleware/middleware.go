f2151501d93144e3c3060e6517d7720fc8ad325a
Nirmal Almara <hello@nlmara.com>
2019-06-27 19:59:06 +0530

    cookie workout - not complete

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -13,7 +13,7 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if origin := c.Request.Header.Get("Origin"); origin != "" {
-			c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+			c.Writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
@@ -46,6 +46,7 @@ func UserMiddleware(db *sql.DB) gin.HandlerFunc {
 		}
 
 		token, _ := c.Cookie("sorcia-token")
+		fmt.Println(token)
 		userID := auth.GetUserIDFromToken(db, token)
 
 		userPresent := false
