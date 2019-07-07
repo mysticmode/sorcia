9e26815d5fdaed85fe56d7843e3e80042406f578
Nirmal Almara <nirmal.almara@outlook.com>
2019-07-07 19:10:39 +0530

    cError to errorhandler convention

--- model/auth.go
+++ model/auth.go
@@ -3,13 +3,13 @@ package model
 import (
 	"database/sql"
 
-	cError "sorcia/error"
+	errorhandler "sorcia/error"
 )
 
 // CreateAccount ...
 func CreateAccount(db *sql.DB) {
 	_, err := db.Query("CREATE TABLE IF NOT EXISTS account (id BIGSERIAL PRIMARY KEY, username VARCHAR(255) UNIQUE NOT NULL, email VARCHAR(255) UNIQUE NOT NULL, password_hash varchar(255) NOT NULL, jwt_token VARCHAR(255) NOT NULL, is_admin BOOLEAN NOT NULL DEFAULT FALSE)")
-	cError.CheckError(err)
+	errorhandler.CheckError(err)
 }
 
 // CreateAccountStruct struct
@@ -26,19 +26,19 @@ func InsertAccount(db *sql.DB, cas CreateAccountStruct) {
 	var lastInsertID int
 
 	err := db.QueryRow("INSERT INTO account (username, email, password_hash, jwt_token, is_admin) VALUES ($1, $2, $3, $4, $5) returning id", cas.Username, cas.Email, cas.PasswordHash, cas.Token, cas.IsAdmin).Scan(&lastInsertID)
-	cError.CheckError(err)
+	errorhandler.CheckError(err)
 }
 
 // GetUserIDFromToken ...
 func GetUserIDFromToken(db *sql.DB, token string) int {
 	rows, err := db.Query("SELECT id FROM account WHERE jwt_token = $1", token)
-	cError.CheckError(err)
+	errorhandler.CheckError(err)
 
 	var userID int
 
 	if rows.Next() {
 		err := rows.Scan(&userID)
-		cError.CheckError(err)
+		errorhandler.CheckError(err)
 	}
 	rows.Close()
 
@@ -48,13 +48,13 @@ func GetUserIDFromToken(db *sql.DB, token string) int {
 // GetUsernameFromToken ...
 func GetUsernameFromToken(db *sql.DB, token string) string {
 	rows, err := db.Query("SELECT username FROM account WHERE jwt_token = $1", token)
-	cError.CheckError(err)
+	errorhandler.CheckError(err)
 
 	var username string
 
 	if rows.Next() {
 		err := rows.Scan(&username)
-		cError.CheckError(err)
+		errorhandler.CheckError(err)
 	}
 	rows.Close()
 
@@ -76,13 +76,13 @@ type SelectPasswordHashAndJWTTokenResponse struct {
 func SelectPasswordHashAndJWTToken(db *sql.DB, sphjwt SelectPasswordHashAndJWTTokenStruct) *SelectPasswordHashAndJWTTokenResponse {
 	// Search for username in the 'account' table with the given string
 	rows, err := db.Query("SELECT password_hash, jwt_token FROM account WHERE username = $1", sphjwt.Username)
-	cError.CheckError(err)
+	errorhandler.CheckError(err)
 
 	var sphjwtr SelectPasswordHashAndJWTTokenResponse
 
 	if rows.Next() {
 		err := rows.Scan(&sphjwtr.PasswordHash, &sphjwtr.Token)
-		cError.CheckError(err)
+		errorhandler.CheckError(err)
 	}
 	rows.Close()
 
