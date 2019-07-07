88d7d489dae822de3cd7f0bf21bfe60e7b1b9f5d
Nirmal Almara <hello@nlmara.com>
2019-07-07 19:10:39 +0530

    cError to errorhandler convention

--- model/repo.go
+++ model/repo.go
@@ -3,13 +3,13 @@ package model
 import (
 	"database/sql"
 
-	cError "sorcia/error"
+	errorhandler "sorcia/error"
 )
 
 // CreateRepo ...
 func CreateRepo(db *sql.DB) {
 	_, err := db.Query("CREATE TABLE IF NOT EXISTS repository (id BIGSERIAL, repo_from INTEGER REFERENCES account(id), name VARCHAR(255) UNIQUE NOT NULL, description VARCHAR(500), is_private BOOLEAN NOT NULL DEFAULT FALSE, PRIMARY KEY (id, repo_from))")
-	cError.CheckError(err)
+	errorhandler.CheckError(err)
 }
 
 // CreateRepoStruct struct
@@ -25,7 +25,7 @@ func InsertRepo(db *sql.DB, crs CreateRepoStruct) {
 	var lastInsertID int
 
 	err := db.QueryRow("INSERT INTO repository (repo_from, name, description, is_private) VALUES ($1, $2, $3, $4) returning id", crs.UserID, crs.Name, crs.Description, crs.IsPrivate).Scan(&lastInsertID)
-	cError.CheckError(err)
+	errorhandler.CheckError(err)
 }
 
 // GetReposFromUserIDResponse struct
@@ -43,14 +43,14 @@ type ReposDetailStruct struct {
 // GetReposFromUserID ...
 func GetReposFromUserID(db *sql.DB, userID int) *GetReposFromUserIDResponse {
 	rows, err := db.Query("SELECT name, description, is_private FROM repository WHERE repo_from = $1", userID)
-	cError.CheckError(err)
+	errorhandler.CheckError(err)
 
 	var grfur GetReposFromUserIDResponse
 	var rds ReposDetailStruct
 
 	for rows.Next() {
 		err := rows.Scan(&rds.Name, &rds.Description, &rds.IsPrivate)
-		cError.CheckError(err)
+		errorhandler.CheckError(err)
 
 		grfur.Repositories = append(grfur.Repositories, rds)
 	}
