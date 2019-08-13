9563f8aba0b3dd59c4f3e844c059fa0a449b75e0
Nirmal Almara <hello@nlmara.com>
2019-08-14 00:49:48 +0530

    execute a shell script gitserve

--- cmd/sshserve.go
+++ cmd/sshserve.go
@@ -16,7 +16,7 @@ var SSHServe = cli.Command{
 }
 
 func runSSH(c *cli.Context) error {
-	sshcmd := exec.Command("./gitserve")
+	sshcmd := exec.Command("/bin/sh", "-c", "./gitserve")
 	err := sshcmd.Run()
 	log.Printf("Command finished with error: %v", err)
 
