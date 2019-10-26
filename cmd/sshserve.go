d49ff039e8305ec9ed62cb1c6e020295ef44262a
Nirmal Almara <hello@nlmara.com>
2019-10-26 15:47:15 +0530

    bug fix on sshserve cmd

--- cmd/sshserve.go
+++ cmd/sshserve.go
@@ -62,7 +62,7 @@ func runSSH(c *cli.Context) error {
 	}
 
 	var cmdSSHServe *exec.Cmd
-	cmdSSHServe = exec.Command("git-shell", "c", sshCmd)
+	cmdSSHServe = exec.Command("git-shell", "-c", sshCmd)
 	fmt.Println(conf.Paths.RepoPath)
 	// cmdSSHServe.Dir = conf.Paths.RepoPath // This should be repo root path
 	cmdSSHServe.Stdout = os.Stdout
