22eff78a9982fccfe21d408f733301b5e8cf16db
Nirmal Almara <hello@nlmara.com>
2019-11-07 07:01:31 +0530

    bug fix on login path - use login path for register form

--- cmd/core.go
+++ cmd/core.go
@@ -67,14 +67,11 @@ func runWeb(c *cli.Context) error {
 		handler.GetLogin(w, r, db)
 	}).Methods("GET")
 	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		handler.PostLogin(w, r, db, decoder)
+		handler.PostLogin(w, r, db, conf.Paths.DataPath, decoder)
 	}).Methods("POST")
 	r.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		handler.GetLogout(w, r)
 	}).Methods("GET")
-	r.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		handler.PostRegister(w, r, db, conf.Paths.DataPath, decoder)
-	}).Methods("POST")
 	r.HandleFunc("/create-repo", func(w http.ResponseWriter, r *http.Request) {
 		handler.GetCreateRepo(w, r, db)
 	}).Methods("GET")
@@ -103,7 +100,7 @@ func runWeb(c *cli.Context) error {
 	http.Handle("/", r)
 
 	allowedOrigins := []string{"*"}
-	allowedMethods := []string{"POST", "GET"}
+	allowedMethods := []string{"GET", "POST"}
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", conf.Server.HTTPPort), handlers.CORS(handlers.AllowedOrigins(allowedOrigins), handlers.AllowedMethods(allowedMethods))(r)))
 
