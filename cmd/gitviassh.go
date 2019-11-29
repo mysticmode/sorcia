6f352ccf1720640967ab994f13b61db92f5a18ac
Nirmal Almara <hello@nlmara.com>
2019-11-29 15:55:23 +0530

    add basic ssh server

--- cmd/gitviassh.go
+++ cmd/gitviassh.go
@@ -2,63 +2,132 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
+	"io/ioutil"
+	"log"
+	"net"
 	"sorcia/setting"
-	"strings"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-func parseSSHCmd(cmd string) (string, string) {
-	ss := strings.SplitN(cmd, " ", 2)
-	if len(ss) != 2 {
-		return "", ""
-	}
-
-	return ss[0], ss[1]
-}
-
+// RunSSH ...
 func RunSSH(conf *setting.BaseStruct) {
-
-	sshCmd := os.Getenv("SSH_ORIGINAL_COMMAND")
-	if len(sshCmd) == 0 {
-		println("Hi there, You've successfully authenticated, but sorcia does not provide shell access.")
-		println("If this is unexpected, please log in with password and setup sorcia under another user.")
+	// Public key authentication is done by comparing
+	// the public key of a received connection
+	// with the entries in the authorized_keys file.
+	authorizedKeysBytes, err := ioutil.ReadFile("/home/mysticmode/.ssh/authorized_keys")
+	if err != nil {
+		log.Fatalf("Failed to load authorized_keys, err: %v", err)
 	}
 
-	gitVerb, args := parseSSHCmd(sshCmd)
-	fmt.Println(gitVerb)
+	authorizedKeysMap := map[string]bool{}
+	for len(authorizedKeysBytes) > 0 {
+		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// For Windows
-	// gitVerb = strings.Replace(gitVerb, "-", " ", 1)
-	// fmt.Println(gitVerb)
-
-	fmt.Println(args)
-	fmt.Println(sshCmd)
-	repoFullName := strings.ToLower(strings.Trim(args, "'"))
-	repoFields := strings.SplitN(repoFullName, "/", 2)
-	if len(repoFields) != 2 {
-		fmt.Printf("Invalid repository path: %v", args)
-	}
-	ownerName := strings.ToLower(repoFields[0])
-	repoName := strings.TrimSuffix(strings.ToLower(repoFields[1]), ".git")
-	repoName = strings.TrimSuffix(repoName, ".wiki")
-
-	fmt.Println(repoFullName)
-	fmt.Println(ownerName)
-	fmt.Println(repoName)
-
-	if gitVerb != "git-upload-pack" && gitVerb != "git-upload-archive" && gitVerb != "git-receive-pack" {
-		fmt.Println("Unknown git command")
+		authorizedKeysMap[string(pubKey.Marshal())] = true
+		authorizedKeysBytes = rest
 	}
 
-	var cmdSSHServe *exec.Cmd
-	cmdSSHServe = exec.Command("git-shell", "-c", sshCmd)
-	fmt.Println(conf.Paths.RepoPath)
-	// cmdSSHServe.Dir = conf.Paths.RepoPath // This should be repo root path
-	// cmdSSHServe.Stdout = os.Stdout
-	// cmdSSHServe.Stdin = os.Stdin
-	// cmdSSHServe.Stderr = os.Stderr
-	if err := cmdSSHServe.Run(); err != nil {
-		fmt.Printf("Fail to execute git command: %v", err)
+	// An SSH server is represented by a ServerConfig, which holds
+	// certificate details and handles authentication of ServerConns.
+	config := &ssh.ServerConfig{
+		// Remove to disable password auth.
+		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+			// Should use constant-time compare (or better, salt+hash) in
+			// a production setting.
+			if c.User() == "testuser" && string(pass) == "tiger" {
+				return nil, nil
+			}
+			return nil, fmt.Errorf("password rejected for %q", c.User())
+		},
+
+		// Remove to disable public key auth.
+		PublicKeyCallback: func(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
+			if authorizedKeysMap[string(pubKey.Marshal())] {
+				return &ssh.Permissions{
+					// Record the public key used for authentication.
+					Extensions: map[string]string{
+						"pubkey-fp": ssh.FingerprintSHA256(pubKey),
+					},
+				}, nil
+			}
+			return nil, fmt.Errorf("unknown public key for %q", c.User())
+		},
+	}
+
+	privateBytes, err := ioutil.ReadFile("/home/mysticmode/.ssh/id_rsa")
+	if err != nil {
+		log.Fatal("Failed to load private key: ", err)
+	}
+
+	private, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+		log.Fatal("Failed to parse private key: ", err)
+	}
+
+	config.AddHostKey(private)
+
+	// Once a ServerConfig has been configured, connections can be
+	// accepted.
+	listener, err := net.Listen("tcp", "0.0.0.0:1938")
+	if err != nil {
+		log.Fatal("failed to listen for connection: ", err)
+	}
+	nConn, err := listener.Accept()
+	if err != nil {
+		log.Fatal("failed to accept incoming connection: ", err)
+	}
+
+	// Before use, a handshake must be performed on the incoming
+	// net.Conn.
+	conn, chans, reqs, err := ssh.NewServerConn(nConn, config)
+	if err != nil {
+		log.Fatal("failed to handshake: ", err)
+	}
+	log.Printf("logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
+
+	// The incoming Request channel must be serviced.
+	go ssh.DiscardRequests(reqs)
+
+	// Service the incoming Channel channel.
+	for newChannel := range chans {
+		// Channels have a type, depending on the application level
+		// protocol intended. In the case of a shell, the type is
+		// "session" and ServerShell may be used to present a simple
+		// terminal interface.
+		if newChannel.ChannelType() != "session" {
+			newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+			continue
+		}
+		channel, requests, err := newChannel.Accept()
+		if err != nil {
+			log.Fatalf("Could not accept channel: %v", err)
+		}
+
+		// Sessions have out-of-band requests such as "shell",
+		// "pty-req" and "env".  Here we handle only the
+		// "shell" request.
+		go func(in <-chan *ssh.Request) {
+			for req := range in {
+				req.Reply(req.Type == "shell", nil)
+			}
+		}(requests)
+
+		term := terminal.NewTerminal(channel, "> ")
+
+		go func() {
+			defer channel.Close()
+			for {
+				line, err := term.ReadLine()
+				if err != nil {
+					break
+				}
+				fmt.Println(line)
+			}
+		}()
 	}
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/schema v1.1.0
 	github.com/lib/pq v1.2.0
-	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
 	golang.org/x/crypto v0.0.0-20191112222119-e1110fd1c708
 	gopkg.in/ini.v1 v1.51.0
 )
