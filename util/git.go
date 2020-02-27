ee59a518f224a8db0733a89d18e310f5f5eb6934
Nirmal Almara <nirmal.almara@outlook.com>
2020-02-27 10:11:07 +0530

    working pullfromallbranches features + util handler

---
+++ util/git.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	errorhandler "sorcia/error"
+)
+
+// PullFromAllBranches ...
+func PullFromAllBranches(gitDirPath string) {
+	var files []string
+
+	dirSplit := strings.Split(gitDirPath, ".git")
+	workDir := dirSplit[0]
+	refPath := filepath.Join(gitDirPath, "refs", "heads")
+	err := filepath.Walk(refPath, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+	errorhandler.CheckError(err)
+
+	for i := 0; i < len(files); i++ {
+		ss := strings.Split(files[i], "/")
+		branch := ss[len(ss)-1]
+
+		if branch == "heads" {
+			continue
+		}
+
+		// by default fast-forward is allowed. Add + to allow non-fast-forward
+		cmd := exec.Command("git", "pull", "origin", fmt.Sprintf("+%s:%s", branch, branch))
+		cmd.Dir = workDir
+
+		var out, stderr bytes.Buffer
+		cmd.Stderr = &stderr
+		cmd.Stdout = &out
+
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(stderr.String())
+		}
+	}
+}
