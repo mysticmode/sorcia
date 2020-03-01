286af2899ac3aa8db6f85e5602ab5620adba2570
Nirmal Almara <hello@nlmara.com>
2020-03-01 23:24:28 +0530

    get fingerprint and email from id_rsa.pub

--- util/misc.go
+++ util/misc.go
@@ -1,5 +1,14 @@
 package util
 
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
 // IsAlnumOrHyphen ...
 func IsAlnumOrHyphen(s string) bool {
 	for _, r := range s {
@@ -8,4 +17,38 @@ func IsAlnumOrHyphen(s string) bool {
 		}
 	}
 	return true
+}
+
+// SSHFingerPrintAndEmail ...
+func SSHFingerPrintAndEmail(sshPath string) (string, string) {
+	key, err := ioutil.ReadFile(sshPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parts := strings.Fields(string(key))
+	if len(parts) < 2 {
+		log.Fatal("bad key")
+	}
+
+	var email string
+	if parts[2] != "" {
+		email = parts[2]
+	}
+
+	k, err := base64.StdEncoding.DecodeString(parts[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fp := md5.Sum([]byte(k))
+	var fingerPrint string
+	for i, b := range fp {
+		fingerPrint = fmt.Sprintf("%s%02x", fingerPrint, b)
+		if i < len(fp)-1 {
+			fingerPrint = fmt.Sprintf("%s:", fingerPrint)
+		}
+	}
+
+	return fingerPrint, email
 }
