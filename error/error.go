39d39e9e18bda5969bf99d5a05809919d6d9f215
Nirmal Almara <nirmal.almara@outlook.com>
2020-03-13 14:56:25 +0530

    error handler write to error.log as well

--- error/error.go
+++ error/error.go
@@ -1,12 +1,21 @@
 package error
 
 import (
+	"io"
 	"log"
+	"os"
 )
 
 // CheckError ...
 func CheckError(errMessage string, err error) {
 	if err != nil {
+		f, err := os.OpenFile("./error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		if err != nil {
+			log.Fatalf("Error opening file: %v", err)
+		}
+		defer f.Close()
+		wrt := io.MultiWriter(os.Stdout, f)
+		log.SetOutput(wrt)
 		log.Printf("%s: %v", errMessage, err)
 	}
 }
