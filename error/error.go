218e5e098a74b606274297bcd69d53b4a90baa4c
Nirmal Almara <nirmal.almara@outlook.com>
2020-03-13 15:01:15 +0530

    bug fix on errorhandler

--- error/error.go
+++ error/error.go
@@ -9,9 +9,9 @@ import (
 // CheckError ...
 func CheckError(errMessage string, err error) {
 	if err != nil {
-		f, err := os.OpenFile("./error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
-		if err != nil {
-			log.Fatalf("Error opening file: %v", err)
+		f, fError := os.OpenFile("./error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		if fError != nil {
+			log.Fatalf("Error opening error.log file: %v", fError)
 		}
 		defer f.Close()
 		wrt := io.MultiWriter(os.Stdout, f)
