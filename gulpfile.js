98f4acff752efe241ad2522cb53d8c426f5182bb
Nirmal Almara <nirmal.almara@outlook.com>
2020-03-16 19:55:28 +0530

    new feature - style alternative

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,11 @@ var paths = {
         src: 'public/scss/style.scss',
         all: 'public/scss/**/**/*.scss',
         dest: 'public/css/'
+    },
+    style_alternative: {
+        src: 'public/scss_alternative/style_alternative.scss',
+        all: 'public/scss_alternative/**/**/*.scss',
+        dest: 'public/css/'
     }
 }
 
@@ -24,9 +29,28 @@ function style() {
     .pipe(gulp.dest(paths.style.dest));
 }
 
+function style_alternative() {
+    return gulp.src(paths.style_alternative.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(minifyCSS())
+    .pipe(rename('style_alternative.min.css'))
+    .pipe(gulp.dest(paths.style_alternative.dest));
+}
+
 function watch() {
     gulp.watch(paths.style.all, style);
 }
 
+function watch_alternative() {
+    gulp.watch(paths.style_alternative.all, style_alternative)
+}
+
 gulp.task('default', gulp.series(watch));
 gulp.task('style', style);
+
+gulp.task('watch-alternative', gulp.series(watch_alternative))
+gulp.task('style-alternative', style_alternative)
