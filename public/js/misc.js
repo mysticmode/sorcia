0edc3609a913acc0b4a4e02914119c96db8a87f9
Nirmal Almara <hello@nlmara.com>
2020-04-23 13:08:23 +0530

    close create-new-dropdown when blurred

--- public/js/misc.js
+++ public/js/misc.js
@@ -1,5 +1,6 @@
 function triggerCreateDropdown() {
   event.preventDefault()
+  event.stopPropagation()
   var dropdown = document.getElementsByClassName('create-new-dropdown active')
   if (dropdown.length > 0) {
       document.getElementsByClassName('create-new-dropdown')[0].classList.remove('active')
@@ -7,3 +8,7 @@ function triggerCreateDropdown() {
       document.getElementsByClassName('create-new-dropdown')[0].classList.add('active')
   }
 }
+
+document.addEventListener('click', function() {
+  document.getElementsByClassName('create-new-dropdown')[0].classList.remove('active')
+})
